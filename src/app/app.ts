206541ced6146edb264edbe0db90ab5654c75fdc
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2026-01-13 21:40:57 -0800

    Added List of todos and property view mode to switch how sticky notes are displayed using note and list item components. Currently header with view mode buttons and test todo items are displayed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,64 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Todo } from './todo/todo';
+import { Task } from './types';
+import { TodoListItem } from "./todo-list-item/todo-list-item";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Todo, RouterOutlet, TodoListItem],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
+
 export class App {
   protected readonly title = signal('todo-app');
+
+  view_mode: "grid" | "list" = 'grid';
+
+  setViewMode(mode: "grid" | "list") {
+    this.view_mode = mode;
+  }
+
+  todos: Task[] = [{
+    title: "Hello World",
+    description: "Do this",
+    completed: false,
+    created_date: new Date(),
+    color: "sticky-yellow"
+  },{
+    title: "Hello World",
+    description: "Do this",
+    completed: false,
+    created_date: new Date(),
+    color: "sticky-purple"
+  },{
+    title: "Hello World",
+    description: "Do this",
+    completed: false,
+    created_date: new Date(),
+    color: "sticky-green"
+  },{
+    title: "Hello World",
+    description: "Do this",
+    completed: false,
+    created_date: new Date(),
+    color: "sticky-blue"
+  },{
+    title: "Hello World",
+    description: "Do this",
+    completed: false,
+    created_date: new Date(),
+    color: "sticky-orange"
+  },{
+    title: "Hello World",
+    description: "Do this",
+    completed: false,
+    created_date: new Date(),
+    color: "sticky-pink"
+  }];
+
+
+
 }
