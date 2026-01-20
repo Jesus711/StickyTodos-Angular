9496a668f714a37a1d6254fa7c6e080bb92053c4
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2026-01-19 20:21:15 -0800

    Added showNoteModal property. Created methods to modified list of todos. Added event emitters for delete and update methods. Modified new note button to display form component. Removed cdkDrag classes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, ChangeDetectorRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Todo } from './todo/todo';
-import { Task } from './types';
+import { StickyNote } from './types';
 import { TodoListItem } from "./todo-list-item/todo-list-item";
+import { CdkDrag, CdkDropList, moveItemInArray, CdkDragDrop } from '@angular/cdk/drag-drop';
+import { TodoForm } from './todo-form/todo-form';
 
 @Component({
   selector: 'app-root',
-  imports: [Todo, RouterOutlet, TodoListItem],
+  imports: [Todo, RouterOutlet, TodoListItem, CdkDrag, CdkDropList,TodoForm],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -17,48 +19,63 @@ export class App {
 
   view_mode: "grid" | "list" = 'grid';
 
+  showNoteModal: boolean = false;
+
   setViewMode(mode: "grid" | "list") {
     this.view_mode = mode;
   }
 
-  todos: Task[] = [{
+
+  todos: StickyNote[] = [{
+    id: 2,
     title: "Hello World",
     description: "Do this",
     completed: false,
     created_date: new Date(),
     color: "sticky-yellow"
   },{
+    id: 3,
     title: "Hello World",
     description: "Do this",
     completed: false,
     created_date: new Date(),
     color: "sticky-purple"
   },{
+    id: 4,
     title: "Hello World",
     description: "Do this",
     completed: false,
     created_date: new Date(),
     color: "sticky-green"
   },{
+    id: 5,
     title: "Hello World",
     description: "Do this",
     completed: false,
     created_date: new Date(),
     color: "sticky-blue"
-  },{
-    title: "Hello World",
-    description: "Do this",
-    completed: false,
-    created_date: new Date(),
-    color: "sticky-orange"
-  },{
-    title: "Hello World",
-    description: "Do this",
-    completed: false,
-    created_date: new Date(),
-    color: "sticky-pink"
   }];
 
 
+  // drop(event: CdkDragDrop<StickyNote[]>) {
+  //   moveItemInArray(this.todos, event.previousIndex, event.currentIndex)
+  // }
+
+  deleteSticky(id: number) {
+    this.todos = this.todos.filter((item) => item.id != id);
+    return id;
+  }
+
+  updateCompleteStatus(id: number) {
+    this.todos = this.todos.map((todo) => todo.id === id ? {...todo, completed: !todo.completed} : todo)
+  }
+
+  createNewSticky(newTodo: StickyNote) {
+    this.todos.push(newTodo);
+  }
+
+  toggleNoteModal(): void {
+    this.showNoteModal = !this.showNoteModal;
+  }
 
 }
