cfb9a70ef103bbd0cbb266fdf155bef40ad093fa
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2026-01-20 21:55:21 -0800

    Modified todos list to be liveQuery observable. Able to create new notes. Modified crud functions to work with live query. Last view mode set is now stored in local storage and will be initialized was last value. Added delete and update event emitters to list item component. Added empty decorators to display message if no notes exist.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,77 +1,82 @@
-import { Component, signal, ChangeDetectorRef } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Todo } from './todo/todo';
 import { StickyNote } from './types';
 import { TodoListItem } from "./todo-list-item/todo-list-item";
 import { CdkDrag, CdkDropList, moveItemInArray, CdkDragDrop } from '@angular/cdk/drag-drop';
 import { TodoForm } from './todo-form/todo-form';
+import { TodoService } from './services/todo.service';
+import { liveQuery } from 'dexie';
+import { AsyncPipe, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [Todo, RouterOutlet, TodoListItem, CdkDrag, CdkDropList,TodoForm],
+  imports: [Todo, TodoListItem, TodoForm, AsyncPipe, NgClass],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 
 
 export class App {
-  protected readonly title = signal('todo-app');
 
+  todos = liveQuery(() => this.todoService.getAllNotes())
   view_mode: "grid" | "list" = 'grid';
+
+  constructor(private todoService: TodoService) {
+    const prevView = window.localStorage.getItem("view-mode")
+    if (prevView !== null && (prevView === "grid" || prevView === "list")) {
+      this.setViewMode(prevView)
+    }
+  }
 
   showNoteModal: boolean = false;
 
   setViewMode(mode: "grid" | "list") {
     this.view_mode = mode;
+    window.localStorage.setItem("view-mode", mode);
   }
-
-
-  todos: StickyNote[] = [{
-    id: 2,
-    title: "Hello World",
-    description: "Do this",
-    completed: false,
-    created_date: new Date(),
-    color: "sticky-yellow"
-  },{
-    id: 3,
-    title: "Hello World",
-    description: "Do this",
-    completed: false,
-    created_date: new Date(),
-    color: "sticky-purple"
-  },{
-    id: 4,
-    title: "Hello World",
-    description: "Do this",
-    completed: false,
-    created_date: new Date(),
-    color: "sticky-green"
-  },{
-    id: 5,
-    title: "Hello World",
-    description: "Do this",
-    completed: false,
-    created_date: new Date(),
-    color: "sticky-blue"
-  }];
-
 
   // drop(event: CdkDragDrop<StickyNote[]>) {
   //   moveItemInArray(this.todos, event.previousIndex, event.currentIndex)
   // }
 
   deleteSticky(id: number) {
-    this.todos = this.todos.filter((item) => item.id != id);
-    return id;
+    try {
+      this.todoService.deleteNote(id);
+      return id;
+    } catch (error) {
+      console.log("Error when trying to delete")
+      return -1;
+    }
   }
 
-  updateCompleteStatus(id: number) {
-    this.todos = this.todos.map((todo) => todo.id === id ? {...todo, completed: !todo.completed} : todo)
+  async updateCompleteStatus(id: number) {
+    try {
+      let currentNote = await this.todoService.getNoteByID(id)
+      if(currentNote !== undefined) {
+        await this.todoService.updateNote({...currentNote, completed: !currentNote.completed});
+      }
+      else {
+        throw new Error("Note does not exist with that ID")
+      }
+    } catch (error) {
+      console.log("error occurred when trying to update")
+    }
   }
 
-  createNewSticky(newTodo: StickyNote) {
-    this.todos.push(newTodo);
+  async updateNote(updatedNote: StickyNote) {
+    const updated = await this.todoService.updateNote(updatedNote);
+    return updated;
+  }
+
+  async createNewSticky(newTodo: StickyNote) {
+    try {
+      await this.todoService.addNote(newTodo);
+      console.log(this.todos.getValue)
+    } catch (error) {
+      console.log("Error occurend when trying to add new note!")
+    }
+
   }
 
   toggleNoteModal(): void {
