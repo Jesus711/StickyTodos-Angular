23df71a5c358e3ae544f61d69a652ac6cef714a2
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2026-01-26 20:49:39 -0800

    Implemented List creation. Validates title to be unique and at least 3 characters in length. If invalid, error message appears below input. Implemented list deleteion and added confirmation to deleting list

--- src/app/sidebar-list/sidebar-list.ts
+++ src/app/sidebar-list/sidebar-list.ts
@@ -1,18 +1,36 @@
-import { Component, input } from '@angular/core';
+import { Component, input} from '@angular/core';
 import { NoteList } from '../types';
 import { TodoService } from '../services/todo.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidebar-list.html',
   styleUrl: './sidebar-list.css',
 })
 export class SidebarList {
   list = input.required<NoteList>();
 
+  toggleModal = false;
 
   constructor(private todoService: TodoService) {
+  }
+
+  openDeleteModal() {
+    if (this.list().protected){
+      console.log("Can't DELETE THIS ONE")
+      return;
+    }
+    this.toggleModal = true;
+  }
+
+  deleteList(confirmed: boolean) {
+    if(confirmed){
+      this.todoService.deleteList(this.list().id!)
+      console.log("DELETED")
+    }
+    this.toggleModal = false;
   }
 
 }
--- src/app/sidebar/sidebar.ts
+++ src/app/sidebar/sidebar.ts
@@ -1,13 +1,14 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, model, output, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NoteList } from '../types';
 import { liveQuery } from 'dexie';
 import { TodoService } from '../services/todo.service';
 import { SidebarList } from '../sidebar-list/sidebar-list';
+import { FormField, form, minLength, required} from '@angular/forms/signals';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [CommonModule, SidebarList],
+  imports: [CommonModule, SidebarList, FormField],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
@@ -19,6 +20,58 @@ export class Sidebar {
 
   closeSidebar = output();
 
+  showListForm = false;
+
+  errorMessage = signal<string | null>(null);
+
+  listModel = signal<NoteList>({
+    title: "",
+    protected: false,
+    noteCount: 0
+  })
+
+  listForm = form(this.listModel, (f) => {
+    required(f.title);
+    minLength(f.title, 3, {message: "Must be at least 3 characters long"});
+    f.protected;
+    f.noteCount;
+  })
+
+
   constructor(private todoService: TodoService) {}
+
+  onSubmit(event: Event) {
+    event.preventDefault()
+    if (this.listForm().valid()) {
+      console.log("VALID FORM PROCEEDING!!!")
+      this.todoService.addList(this.listModel()).then(() => {
+        this.clearName()
+        this.toggleListForm()
+        console.log("SUCCESS")
+      }).catch(error => {
+        if(error.name === "ConstraintError") {
+          this.errorMessage.set(`'${this.listForm.title().value()}' list already exists!`)
+        }
+        else {
+          console.log(error)
+        }
+      })
+    }
+  }
+
+  clearName() {
+    if(this.listForm.title().value() === ""){
+      return;
+    }
+    this.listModel.set({
+    title: "",
+    protected: false,
+    noteCount: 0
+    })
+  }
+
+  toggleListForm() {
+    this.showListForm = !this.showListForm;
+  }
 
 }
